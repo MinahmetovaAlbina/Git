ba7957e3880ffbc9d59b5428ea7b4e76cd901301
Mimahmetova_Albina_11-808 <wildFlow14@gmail.com>
2018-09-14 19:52:16 +0300

    add Expr1

---
+++ 2018/FALL/SEM/Expr1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Минахметова Альбина 11-808
+//Поменять местами значения двух переменных без промежуточной  переменной
+using System;
+
+namespace Expr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            secondNum -= firstNum;
+            firstNum += secondNum;
+            secondNum = firstNum - secondNum;
+            Console.WriteLine(firstNum);
+            Console.WriteLine(secondNum);
+        }
+    }
+}
