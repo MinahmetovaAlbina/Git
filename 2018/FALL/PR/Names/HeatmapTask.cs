6562f797b36ca166c8ef1c65a7c2fc508cfcd0b3
Mimahmetova_Albina_11-808 <wildFlow14@gmail.com>
2018-10-09 21:08:05 +0300

    add names

---
+++ 2018/FALL/PR/Names/HeatmapTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerYearHeatmap(NameData[] names)
+        {
+            //choose a range
+            var maxYear = 1995;
+            var minYear = 1945;
+            var years = new string[maxYear - minYear + 1];
+            for (var y = 0; y < years.Length; y++)
+                years[y] = (y + minYear).ToString();
+
+            //create new array of names
+            string[] group = File.ReadAllLines(@"group.txt");
+            Array.Sort(group);
+
+            //map an index to each name
+            var dictionary = new Dictionary<string, int>();
+            for (var i = 0; i<group.Length;i++)
+                dictionary[group[i]] = i;
+
+            //calculate and fill an array for the amount of born ones
+            var birthsCounts = new double[years.Length, group.Length];
+            foreach (var e in names)
+            {
+                var nameYear = e.BirthDate.Year;
+                if (dictionary.ContainsKey(e.Name) && minYear <= nameYear && nameYear <= maxYear)
+                    birthsCounts[nameYear - minYear, dictionary[e.Name]]++;
+            }
+            return new HeatmapData(string.Format("Тепловая карта c {0} по {1} гг.", minYear, maxYear), birthsCounts, years, group);
+        }
+    }
+}
---
+++ 2018/FALL/PR/Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Names
+{
+    public static class Program
+    {
+        private static readonly string dataFilePath = "names.txt";
+
+        private static void Main(string[] args)
+        {
+             var namesData = ReadData();
+             Charts.ShowHeatmap(HeatmapTask.GetBirthsPerYearHeatmap(namesData));
+             Console.WriteLine();
+        }
+        
+        private static NameData[] ReadData()
+        {
+            return File
+                .ReadLines(dataFilePath)
+                .Select(NameData.ParseFrom)
+                .ToArray();
+        }
+    }
+}
